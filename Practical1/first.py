0d8876b22512a40a70a4720f791fe0a257d20bd6
Jeet Jawale <jeetjawale03@gmail.com>
2023-06-29 22:32:56 +0530

    Create first.py

---
+++ Practical1/first.py
@@ -0,0 +1,49 @@
+a = "hello"
+print(a)
+
+b = """ This is a practical 
+ """
+print(b)
+
+a = "hello, world!"
+print(a[1:4])
+
+for x in "banana":
+    print(x)
+
+a = "hello, world!"
+print(len(a))
+
+txt = "The best thing in my life are free"
+print("free" in txt)
+
+txt = "The best thing in my life are free"
+if "free" in txt:
+    print("Yes, 'free' is present")
+
+txt = "The best thing in my life are free"
+print("do" not in txt)
+
+txt = "The best thing in my life are free"
+if "student" not in txt:
+    print("Not in txt")
+
+a = "hello, world!"
+print(a[1:4])
+
+a = "hello, world!"
+print(a[:5])
+
+a = "hello, world!"
+print(a[7:])
+
+print(a[-6:-2])
+
+print(a.upper())
+print(a.lower())
+
+print(a.strip())
+
+print(a.replace("h", "j"))
+
+print(a.split(","))
